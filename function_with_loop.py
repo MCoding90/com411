df4f84a2cab5e6a2e3ab999bb0c16763262b60dd
Maciej Czerwonka <5czerm04@solent.ac.uk>
2021-10-14 11:39:44 +0100

    Added code to demonstrate use of function with loop

---
+++ function_with_loop.py
@@ -0,0 +1,12 @@
+def cross_bridge():
+    print("Crossed step")
+    distance = input()
+
+    if distance > 5 :
+        print("The bridge is collapsing")
+
+    else:
+        print("We must keep going")
+
+cross_bridge()
+cross_bridge()
