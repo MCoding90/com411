2c69cdaeae4424f54da7eaa6e8d800a595b01fd3
czerw <czerwonkaster@gmail.com>
2021-12-09 12:23:59 +0000

    Updated classes human and robot with methods to display objects.

--- oop/human.py
+++ oop/human.py
@@ -1,8 +1,7 @@
-
 class Human:
     MAX_ENERGY = 100
 
-    def __init__(self,name = "Human", age=0):
+    def __init__(self, name="Human", age=0):
         self.name = name
         self.age = age
         self.energy = Human.MAX_ENERGY
@@ -11,6 +10,17 @@ class Human:
     def display(self):
         print(f"I am {self.name}")
 
+    # magic methods
+    def __repr__(self):
+        return f'human(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old and my energy is {self.energy}.'
+
+
 if (__name__ == "__main__"):
     human = Human()
     human.display()
+    print(repr(human))
+    print(str(human))
+
--- oop/robot.py
+++ oop/robot.py
@@ -1,16 +1,25 @@
-
 class Robot:
     MAX_ENERGY = 100
 
-    def __init__(self,name = "Robot",age = 0):
+    def __init__(self, name="Robot", age=0):
         self.name = name
         self.age = age
         self.energy = Robot.MAX_ENERGY
 
-# instance methods
+    # instance methods
     def display(self):
         print(f"I am {self.name}")
+
+    # magic methods
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old and my energy is {self.energy}.'
+
 
 if (__name__ == "__main__"):
     robot = Robot()
     robot.display()
+    print(repr(robot))
+    print(str(robot))
