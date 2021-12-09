f4a7b4b35af90a4daabed81d232cafa1e2b0fa7c
czerw <czerwonkaster@gmail.com>
2021-12-09 12:30:09 +0000

    Updated classes human and robot with instance methods.

--- oop/robot.py
+++ oop/robot.py
@@ -10,6 +10,27 @@ class Robot:
     def display(self):
         print(f"I am {self.name}")
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        potential_energy = self.energy + amount
+        if potential_energy > Robot.MAX_ENERGY:
+            self.energy = Robot.MAX_ENERGY
+            return potential_energy - self.energy
+        else:
+            self.energy = potential_energy
+            return 0
+
+    def move(self, distance):
+        potential_energy = self.energy - distance
+        if potential_energy < 0:
+            self.energy = 0
+            return self.energy - abs(potential_energy)
+        else:
+            self.energy = potential_energy
+            return 0
+
     # magic methods
     def __repr__(self):
         return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
