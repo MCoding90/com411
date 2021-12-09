6ac38f8e20911eb8771210bfef16f52a6cffc9cd
czerw <czerwonkaster@gmail.com>
2021-12-09 12:07:27 +0000

    Added code to create basic human and robot classes.

---
+++ oop/human.py
@@ -0,0 +1,16 @@
+
+class Human:
+    MAX_ENERGY = 100
+
+    def __init__(self,name = "Human", age=0):
+        self.name = name
+        self.age = age
+        self.energy = Human.MAX_ENERGY
+
+    # instance methods
+    def display(self):
+        print(f"I am {self.name}")
+
+if (__name__ == "__main__"):
+    human = Human()
+    human.display()
